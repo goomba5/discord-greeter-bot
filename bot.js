5e800bc01490f399f7bb471f36082da51aba18a7
Gary Henderson <hendergy@gmail.com>
2019-10-17 14:03:23 -0400

    created commandFiles to allow access to each command module

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
+const fs = require("fs");
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const { token, prefix } = require('./auth.json');
 
-// command enums
-const CommandEnum = Object.freeze({ "strike": "strike", "info": "info", "ping": "ping", "server": "server" });
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
