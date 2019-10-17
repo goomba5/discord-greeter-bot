297ccb87d421f1aae262865a184d884b129cf3c0
Gary Henderson <hendergy@gmail.com>
2019-10-17 11:17:15 -0400

    added info command with argument split

--- bot.js
+++ bot.js
@@ -1,20 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const { prefix } = require('./auth.json');
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
-client.login(auth.token);
-
 client.on("message", msg => {
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
     if (msg.content === "ping") {
-        msg.reply("pong");
+        return msg.reply("pong");
     }
-    if (msg.content === "Strike!") {
-        msg.reply("We've stepped into a war with the Cabal. Let's get to taking out their command one by one.")
+    else if (msg.content === "strike") {
+        return msg.reply("we've stepped into a war with the Cabal. Let's get to taking out their command one by one.")
+    }
+
+    if (!msg.content.startsWith(prefix) || msg.author.bot) {
+        return
+    }
+    else if (command === "info") {
+        if (!args.length) {
+            return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
+        } else {
+            return msg.channel.send(`Commannd name: ${command}\nArguments: ${args}`);
+        }
+
     }
 })
+
+
 
 client.login(auth.token);
